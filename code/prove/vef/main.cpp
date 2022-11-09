f6caa4052ee6e915bd265a3639e8e5a2019825a4
Lu-Igi <gue.luigi@libero.it>
2022-11-09 13:53:41 +0100

    Corretto il l'incremento della variabile a, ora è impossibile insserire più di 10 elementi che non rispettano  il range 1-10.

--- code/prove/vef/main.cpp
+++ code/prove/vef/main.cpp
@@ -4,59 +4,73 @@ using namespace std;
 
 int main()
 {
-int gruppo[10], gruppo1[10], gruppo2[10];
+int vettore1[10], vettore2[10], vettore3[10];
+/*VARIABILI*/
 int a, b, c, risultato;
-
+/*a,b,c;.variabile che identifichiamo come indice*/
+/*risultato viene utilizzato come variabile di appoggio come contenitore*/
 do{
 cout<<"Inserisci dei valori compresi tra 1-10" <<endl;
-cin>>gruppo[a];
+cin>>vettore1[a];
 
-if(gruppo[a]<=5)
-{
+if(vettore1[a]<=5)
+{/*In questa istruzione viene fatto il controllo del range*/
 risultato=0;
-risultato = gruppo[a]*4;
+risultato = vettore1[a]*4;
 cout<<"Il valore moltiplicato per 4 è " <<risultato<<endl;
-gruppo1[b]=risultato;
+vettore2[b]=risultato;
+a++;
 b++;
 }
-else if(gruppo[a] >= 6 && gruppo[a] <= 10)
-{
+else if(vettore1[a] >= 6 && vettore1[a] <= 10)
+{/*In questa istruzione viene fatto il controllo del range*/
 risultato = 0;
-risultato = gruppo[a]*3;
+risultato = vettore1[a]*3;
 cout<<"Il valore moltiplicato per 3 è " << risultato<<endl;
-gruppo2[c]=risultato;
+vettore3[c]=risultato;
+a++;
 c++;
 }
+/*else-Il caso in cui un valore inserito è superiore al range che vogliamo gestire
+darà un mess. di errore*/
 else
 {
-cout<<"Attenzione il valore "<<gruppo[a]<<" non e' accettabile " <<endl;
+cout<<"Attenzione il valore "<<vettore1[a]<<" non e' accettabile " <<endl;
 }
-a++;
+/*L'indice a una doppia funzionalità
+1.viene incrementato per non sovrascrivere i valori nel vettore
+2.serve per uscire dal while*/
+
 }
+/*il while controlla se la condizionde della variabile che identifica l'indice
+e minore di 10 in modo da terminare il ciclo do-while*/
 while(a<10);
 
 cout<<endl;
-cout<<"Vettore originale 1 "<<endl;
+cout<<"1° Vettore originale"<<endl;
+
+/*Inseriamo tre cicli for per visualizzare la
+posizione dei tre vettori */
 
 for(a=0;a<10;a++)
 {
-cout<<gruppo[a]<<" ";
+cout<<vettore1[a]<<" ";
 }
 
 cout<<endl;
-cout<<"Vettore originale 2"<<endl;
+cout<<"2° Vettore che motilica *4"<<endl;
 
 for(b=0;b<10;b++)
 {
-cout<<gruppo1[b]<<" ";
+cout<<vettore2[b]<<" ";
 }
 
 cout<<endl;
-cout<<"Vettore originale 3"<<endl;
+cout<<"3° Vettore che moltiplica *3"<<endl;
 
 for(c=0;c<10;c++)
 {
-cout<<gruppo2[c]<<" ";
+cout<<vettore3[c]<<" ";
 }
 
 cout<<endl;
